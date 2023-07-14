4e0aa9016bcc9e833e147a33800b7fe5b1c4594a
emhamza <imumairhamza@gmail.com>
2023-07-14 09:52:31 +0500

    Add tests for custom methods

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:hamza) { User.create(name: 'Umair Hamza', photo: 'photo link here', bio: 'Bio text here', posts_count: 0) }
-
-  subject do
-    Post.new(
-      title: 'Five ways to learn Ruby',
-      text: 'Ruby is a convention-based language',
-      author: hamza
-    )
-  end
+  let(:user) { User.create(name: 'John Doe', posts_count: 0) }
+  subject { described_class.new(title: 'Sample Post', author: user) }
 
   before { subject.save }
 
@@ -23,13 +16,46 @@ RSpec.describe Post, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it 'should not have a negative integer for the comments counter' do
+  it 'title length should be between 1 and 250 characters' do
+    subject.title = ''
+    expect(subject).to_not be_valid
+
+    subject.title = 'A' * 251
+    expect(subject).to_not be_valid
+  end
+
+  it 'likes counter should not be less than 0' do
+    subject.likes_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'comment counter should not be less than 0' do
     subject.comment_counter = -1
     expect(subject).to_not be_valid
   end
 
-  it 'should not have a negative integer for the likes counter' do
-    subject.likes_counter = -1
-    expect(subject).to_not be_valid
+  describe 'scopes' do
+    describe '.get_5_comments' do
+      let!(:post) { Post.create(title: 'Sample Post', author: user) }
+      let!(:recent_comments) { create_comments(post, 5, 1.day.ago) }
+      let!(:older_comments) { create_comments(post, 3, 2.days.ago) }
+
+      it 'returns the five most recent comments for the post' do
+        expect(Post.get_5_comments(post)) == (recent_comments.reverse)
+      end
+
+      it 'limits the result to five comments' do
+        expect(Post.get_5_comments(post).count) == (5)
+      end
+    end
+  end
+
+  # Helper method to create comments
+  def create_comments(post, count, created_at)
+    comments = []
+    count.times do
+      comments << Comment.create(post:, created_at:)
+    end
+    comments
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Umair Hamza', photo: 'link to photo', bio: 'Bio text here', posts_count: 0) }
+  subject { described_class.new(name: 'Umair Hamza', photo: 'link to photo', bio: 'Bio text here', posts_count: 0) }
 
   before { subject.save }
 
@@ -20,16 +20,25 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
-  # describe '#three_recent_posts'  do
-  #   let(:user) { create(:user) }
+  describe 'scopes' do
+    describe '.three_recent_posts' do
+      let(:user) { described_class.create(name: 'John Doe', posts_count: 0) }
+      let!(:recent_post) { Post.create(author: user, created_at: 1.day.ago) }
+      let!(:older_post1) { Post.create(author: user, created_at: 2.days.ago) }
+      let!(:older_post2) { Post.create(author: user, created_at: 3.days.ago) }
+      let!(:another_user_post) { Post.create(created_at: 1.day.ago) }
 
-  #   it 'returns the three most recent posts' do
-  #     user = User.create(name: 'name')
-  #     first_post = Post.create(title: 'first_post', text: 'text', author_id: user.id)
-  #     Post.create(title: 'second_post', text: 'text', author_id: user.id)
-  #     Post.create(title: 'third_post', text: 'text', author_id: user.id)
-  #     Post.create(title: 'fourth_post', text: 'text', author_id: user.id)
-  #     expect(user.three_recent_posts).to_not include(first_post)
-  #   end
-  # end
+      it 'returns the three most recent posts by the user' do
+        expect(User.three_recent_posts(user)) == ([recent_post, older_post1, older_post2])
+      end
+
+      it 'does not include posts by other users' do
+        expect(User.three_recent_posts(user)).not_to include(another_user_post)
+      end
+
+      it 'limits the result to three posts' do
+        expect(User.three_recent_posts(user).count) == (3)
+      end
+    end
+  end
 end
