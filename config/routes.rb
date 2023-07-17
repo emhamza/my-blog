170165be595ddb8da55d38c438245fec052be7ab
emhamza <imumairhamza@gmail.com>
2023-07-17 15:12:32 +0500

    Create controllers and routes for my blog app

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+
+  get '/users', to: 'users#index'
+
+  get '/users/:id', to: 'users#show'
+
+  get '/users/:id/posts', to: 'posts#index'
+
+  get '/users/:id/posts/:posts_id', to: 'posts#show'
+
 end
