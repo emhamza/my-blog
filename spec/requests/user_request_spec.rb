9a125f189eb74f015f941978649d374a79eddc24
emhamza <imumairhamza@gmail.com>
2023-07-18 11:44:31 +0500

    Add controller specs

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :request do
+  describe 'users controller' do
+    it 'should render the users index' do
+      get users_path
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('users/index')
+      expect(response.body).to include('This file is for User list')
+    end
+
+    it 'should render the user show' do
+      get '/users/:id'
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('users/show')
+      expect(response.body).to include('Here goes the users info')
+    end
+  end
 end
